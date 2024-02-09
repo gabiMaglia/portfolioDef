53b86b80e1222e489f6f85ce225b18bbed629ef1
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-02-09 19:44:27 -0300

    pre

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  server  : 'https://portfoliobackfinal-production.up.railway.app'
+  server  : 'https://portfoliobe-au8o.onrender.com'
   // server  : 'http://portfoliobackend-env.eba-iepf8x2c.us-east-1.elasticbeanstalk.com'
 };
