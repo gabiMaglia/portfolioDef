b69be9997af2c2bf2417ed31a0e335133f3ff1d3
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-02-09 18:14:41 -0300

    cambie flechas por indicadores en el carusel

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 // import { FrontPageComponent } from './components/front-page/front-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SeparatorComponent } from './components/common/separator/separator.component';
@@ -21,6 +21,8 @@ import { ModalComponent } from './components/common/modal/modal.component';
 import { ArgProgLogoComponent } from './components/common/arg-prog-logo/arg-prog-logo.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ResumeComponent } from './components/common/resume/resume.component';
+import { ImageGalleryModalComponent } from './components/image-gallery-modal/image-gallery-modal.component';
+// import { ImageGalleryModalComponent } from './components/image-gallery-modal/image-gallery-modal.component';}
 
 
 
@@ -41,14 +43,17 @@ import { ResumeComponent } from './components/common/resume/resume.component';
     ModalComponent,
     ArgProgLogoComponent,
     ResumeComponent,
+    ImageGalleryModalComponent,
   ],
   imports: [
+ 
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    NgbCarouselModule,
     BrowserAnimationsModule,
     MatProgressBarModule,
     
