992324440e33e99e9d24253532a63d6f1816bdc7
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-02-09 19:33:38 -0300

    pre

--- Server/build/src/routes/updateRoutes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const updateRouter = (0, express_1.Router)();
-exports.default = updateRouter;
-//# sourceMappingURL=updateRoutes.js.map
