5ce74d5c29c5d84798cd424c2cb74ea93915a7bb
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-02-13 19:14:35 -0300

    pre

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  server  : 'https://portfoliobe-au8o.onrender.com'
+  server  : 'https://portfoliobe-uvew.onrender.com'
   // server  : 'http://portfoliobackend-env.eba-iepf8x2c.us-east-1.elasticbeanstalk.com'
 };
